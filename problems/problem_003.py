d83b05ce19ce60a2ad7ea817d6fafbd3a9bf74ae
gabrielPistore <devgbpistore@gmail.com>
2024-05-15 10:44:31 -0300

    Problem 003 solved

---
+++ problems/problem_003.py
@@ -0,0 +1,17 @@
+# Problem 3. Largest Prime Factor
+
+
+def largest_prime_factor(num):
+    if num == 1:
+        return 1
+
+    factor = 2
+    while factor < num:
+        if num % factor == 0:
+            num /= factor
+        else:
+            factor += 1
+    return factor
+
+
+print(largest_prime_factor(600851475143))
