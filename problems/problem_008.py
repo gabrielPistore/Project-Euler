d9218ad3c989dda32151b8f3cd365a4991a89465
gabrielPistore <devgbpistore@gmail.com>
2024-05-16 14:33:09 -0300

    add problem_008.py and problem_008.txt

--- problems/problem_008.py
+++ problems/problem_008.py
@@ -1,1 +1,15 @@
 # Problem 008. Largest Product in a Series
+
+from pathlib import Path
+
+cwd = Path.cwd()
+
+TXT_PATH = cwd / "problems" / "problem_008.txt"
+
+with open(TXT_PATH, "r") as file:
+    digits = file.read()
+
+products = []  # type: ignore
+
+for i in range(len(digits) - 13):
+    pass
